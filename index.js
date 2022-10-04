59e89340ec4b5520a7214a2741ffa9f857a49df7
Abu Sani Faysal <97028772+SanyFaysal@users.noreply.github.com>
2022-10-04 10:20:53 +0600

    add server port

--- index.js
+++ index.js
@@ -1,17 +1,16 @@
-const mongoose = require("mongoose");
-const dotenv = require("dotenv").config();
-const colors = require("colors");
+const mongoose = require('mongoose');
+const dotenv = require('dotenv').config();
+const colors = require('colors');
 
-const app = require("./app");
+const app = require('./app');
 // database connection
-mongoose.connect(process.env.DATABASE_LOCAL).then(()=>{
+mongoose.connect(process.env.DATABASE_LOCAL).then(() => {
   console.log(`Database is connected successfully `.blue.bold);
-})
+});
 
-// server
+// server port
 const port = process.env.PORT || 8080;
 
 app.listen(port, () => {
   console.log(`App is running on port ${port}`.yellow.bold);
 });
-
